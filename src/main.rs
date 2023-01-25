aaaff25770ac8c9b168b5477b9d85faed642465f
mathijs-bakker <mathijs_bakker@me.com>
2023-01-25 21:13:24 +0100

    Add Game loop. Start new game and game over.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+#![warn(clippy::all, clippy::pedantic)]
+use std::io::Write;
+use std::io::{stdin, stdout};
+
 fn main() {
-    println!("Hello, world!");
+    let mut is_game_over = true;
+
+    loop {
+        start_new_game(&mut is_game_over);
+
+        if is_game_over {
+            println!("Game Over");
+        }
+    }
+}
+
+fn start_new_game(is_game_over: &mut bool) {
+    if *is_game_over {
+        let mut input = String::new();
+        println!("Start? (y/n)");
+        let _ = stdout().flush();
+
+        match stdin().read_line(&mut input) {
+            Ok(_) => {
+                if input == "y\n" {
+                    *is_game_over = false;
+                    println!("Start new game!");
+                }
+            }
+            Err(e) => {
+                println!("Error: {e}");
+            }
+        }
+    }
 }
